12c52385a7a71e80f37d60811a7bb18d6220ad8b
bayan <100108505+bayanyelemes@users.noreply.github.com>
2024-02-09 01:40:56 +0600

    Add files via upload

---
+++ oop.sql
@@ -0,0 +1,11 @@
+INSERT INTO citizens
+VALUES ( 1, "Smith", "Street 23", "London"),
+	   ( 2, "Johnson", "Street 4", "New-York"),
+       ( 3, "Jason", "Street 76", "Amsterdam"),
+       ( 4, "White", "Street 29", "Brooklyn"),
+       ( 6, "Thomas", "Street 15", "Paris"),
+       ( 7, "Anderson", "Street 45", "Rome"),
+       ( 8, "Taylor", "Street 76", "Berlin"),
+       ( 9, "Wilson", "Street 51", "Madrid"),
+       ( 10, "Jackson", "Street 77", "Hamburg");
+SELECT * FROM citizens;       
